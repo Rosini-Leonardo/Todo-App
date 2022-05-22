cc6b639811d97ddae9e651a82e5a4f5173f15832
Rosini Leonardo <96723681+Rosini-Leonardo@users.noreply.github.com>
2022-05-22 09:44:16 +0200

    Update settings.py

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ['todo-wapp.netlify.app']
+ALLOWED_HOSTS = ['yourIP']
 
 
 # Application definition
